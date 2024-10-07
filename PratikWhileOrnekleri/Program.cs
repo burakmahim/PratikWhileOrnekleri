5fdacdeaca23950b6501d64e34c45c4da2f8d176
burakmahim <burakmahim718@gmail.com>
2024-10-07 17:40:51 +0300

    Proje dosyası ekle.

---
+++ PratikWhileOrnekleri/Program.cs
@@ -0,0 +1,60 @@
+﻿
+// 1 -> Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+int sayac = 1;
+while (sayac <= 10)
+{
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+    sayac++;
+}
+
+//2 -> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+int count=1;
+while (count < 20)
+{
+    Console.Write($"{count}  ");
+    count++;
+}
+
+Console.WriteLine();
+
+//3 -> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+int countCift=1;
+while(countCift < 20)
+{
+    if (countCift % 2 == 0)
+    {
+        Console.Write($"{countCift}  ");
+    }
+    countCift++;
+}
+Console.WriteLine();
+
+//4 -> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+int sum =0;
+int sumCount = 50;
+while (sumCount < 150 )
+{
+    sum += sumCount;
+    sumCount++;
+}
+Console.WriteLine($"sayıların toplamı: {sum}");
+
+//5 -> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+int sayacCift = 0;
+int sayacTek =0;
+int sayacFull = 1;
+while (sayacFull < 120)
+{
+    if (sayacFull % 2 == 0)
+    {
+        sayacCift += sayacFull;
+    }
+    else
+    {
+        sayacTek += sayacFull;
+    }
+    sayacFull++;
+}
+
+Console.WriteLine($"çift sayılar toplamı: {sayacCift}");
+Console.WriteLine($"tek sayılar toplamı: {sayacTek}");
---
+++ README.dm
@@ -0,0 +1,13 @@
+# SORULAR
+
+## AÞAÐIDA BULUNAN SORULARI WHÝLE DÖNGÜSÜYLE NASIL YAPILACAÐI GÖSTERÝLMÝÞTÝR.
+
+1. Konsol ekranýna 10 kere "Kendime inanýyorum, ben bu yazýlým iþini hallederim!" yazdýrýnýz.
+
+2. 1 ile 20 arasýndaki sayýlarý konsol ekranýna yazdýrýnýz.
+
+3. 1 ile 20 arasýndaki çift sayýlarý konsol ekranýna yazdýrýnýz.
+
+4. 50 ile 150 arasýndaki sayýlarýn toplamýný ekrana yazdýrýnýz.
+
+5. 1 ile 120 arasýndaki tek ve çift sayýlarýn toplamlarýný ayrý ayrý ekrana yazdýrýnýz.
